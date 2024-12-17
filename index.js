68043f9e25ff1346d0d3a2181dc865d631624e20
MHC32 <hantzmichaelchery6@gmail.com>
2024-12-16 23:18:59 -0500

    structur the project

---
+++ index.js
@@ -0,0 +1,10 @@
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.page = this.page;
+    this.read = this.read;
+    this.info = function () {
+        return this.title + 'by' + this.author + ',' + this.page + 'pages' + this.read + 'read yet'
+    }
+
+}
